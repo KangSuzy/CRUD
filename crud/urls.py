6e90cc18f818d04b298cd525e79a75ebacd262bd
KangSuzy <astnwl321@gmail.com>
2019-08-07 20:52:25 +0900

    add urls.py media path

--- crud/urls.py
+++ crud/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 import crudApp.views
 import accounts.views
+from django.conf import settings
+from djanog.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', crudApp.views.index, name="index"),
     path('crudApp/', include('crudApp.urls')),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
