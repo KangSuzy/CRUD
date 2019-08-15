089460117655bec907d8478276e5a43bc217847a
KangSuzy <astnwl321@gmail.com>
2019-08-15 18:48:16 +0900

    add home button on nav bar

--- crud/settings.py
+++ crud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crudApp.apps.CrudappConfig',
     'accounts.apps.AccountsConfig',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
